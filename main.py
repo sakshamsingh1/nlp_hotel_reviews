dc37d2be36bab573bcd735c46424104b01603bb9
Saksham Singh <sakshamrock121@gmail.com>
2023-10-07 18:36:18 -0500

    add another smoothing and unknown word handler

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import math
 from collections import defaultdict, Counter
 
 class NgramModel:
-    def __init__(self, threshold=1, k=1, ngram=2, smoothing=False):
+    def __init__(self, ngram=2, smoothing=None, unknown_handle=None, threshold=None, k=None, top_k_vocab=None):
         self.unigram_counts = Counter()
         self.bigram_counts = defaultdict(Counter)
         self.vocab = set()
@@ -10,6 +10,9 @@ class NgramModel:
         self.k = k
         self.ngram = ngram
         self.smoothing = smoothing
+        self.top_k_vocab = top_k_vocab
+        self.unknown_handle = unknown_handle
+        self.epsilon = 1e-10
 
     def preprocess(self, text):
         text = text.lower()
@@ -22,11 +25,17 @@ class NgramModel:
                 tokens = self.preprocess(line).strip().split()
                 tokens_with_boundaries = ["<START>"] + tokens + ["<END>"]
                 self.unigram_counts.update(tokens_with_boundaries)
+        if self.unknown_handle is not None:
+            low_freq_words = []
+            if self.unknown_handle == "threshold":
+                low_freq_words = [word for word, count in self.unigram_counts.items() if count <= self.threshold]
+            elif self.unknown_handle == "top_k_vocab":
+                top_k = sorted(self.unigram_counts.items(), key=lambda x: x[1], reverse=True)[:self.top_k_vocab]
+                low_freq_words = [word for word, count in self.unigram_counts.items() if word not in top_k]
 
-        low_freq_words = [word for word, count in self.unigram_counts.items() if count <= self.threshold]
-        for word in low_freq_words:
-            self.unigram_counts["<UNK>"] += self.unigram_counts[word]
-            del self.unigram_counts[word]
+            for word in low_freq_words:
+                self.unigram_counts["<UNK>"] += self.unigram_counts[word]
+                del self.unigram_counts[word]
 
         self.vocab = set(self.unigram_counts.keys())
 
@@ -42,7 +51,12 @@ class NgramModel:
             token = "<UNK>"
 
         total = sum(self.unigram_counts.values())
-        return (self.unigram_counts[token] + self.k * self.smoothing) / (total + len(self.vocab) * self.k * self.smoothing)
+        if self.smoothing is not None:
+            if self.smoothing == "add-k":
+                return (self.unigram_counts[token] + self.k) / (total + len(self.vocab) * self.k)
+            elif self.smoothing == "stupid_backoff":
+                return self.unigram_counts[token] / total if self.unigram_counts[token] else self.epsilon
+        return self.unigram_counts[token] / total
 
     def bigram_probability(self, token1, token2):
         token1, token2 = self.preprocess(token1), self.preprocess(token2)
@@ -51,11 +65,17 @@ class NgramModel:
         if token2 not in self.vocab:
             token2 = "<UNK>"
 
-        if self.smoothing:
-            return (self.bigram_counts[token1][token2] + self.k * self.smoothing) / (self.unigram_counts[token1] + len(self.vocab) * self.k * self.smoothing)
-        else:
-            prob = self.bigram_counts[token1][token2] / self.unigram_counts[token1] if self.unigram_counts[token1] else 0
-            return prob if prob > 0 else 1e-10
+        if self.smoothing is not None:
+            if self.smoothing == "add-k":
+                return (self.bigram_counts[token1][token2] + self.k) / (self.unigram_counts[token1] + len(self.vocab) * self.k)
+            elif self.smoothing == "stupid_backoff":
+                if token1 in self.bigram_counts and token2 in self.bigram_counts[token1]:
+                    return self.bigram_counts[token1][token2] / self.unigram_counts[token1]
+                elif token2 in self.unigram_counts:
+                    return 0.4 * self.unigram_counts[token2] / sum(self.unigram_counts.values())
+                else:
+                    return self.epsilon
+        return self.bigram_counts[token1][token2] / self.unigram_counts[token1]
 
     def perplexity(self, filepath):
         log_sum = 0
@@ -74,12 +94,13 @@ class NgramModel:
 
 def main():
     models = {
-        "Unigram Unsmooth": NgramModel(threshold=1, ngram=1, smoothing=False),
-        "Unigram Smooth with Laplace": NgramModel(threshold=1, ngram=1, k=1, smoothing=True),
-        "Unigram Smooth with Add-2": NgramModel(threshold=1, ngram=1, k=2, smoothing=True),
-        "Bigram Unsmooth": NgramModel(threshold=1, ngram=2, smoothing=False),
-        "Bigram Smooth with Laplace": NgramModel(threshold=1, ngram=2, k=1, smoothing=True),
-        "Bigram Smooth with Add-2": NgramModel(threshold=1, ngram=2, k=2, smoothing=True),
+        "Unigram Unsmooth": NgramModel(ngram=1, smoothing=None, unknown_handle=None, threshold=None, k=None, top_k_vocab=None),
+        "Unigram Smooth with Laplace": NgramModel(ngram=1, smoothing='add-k', unknown_handle=None, threshold=None, k=1, top_k_vocab=None),
+        "Unigram Smooth with stupid_backoff": NgramModel(ngram=1, smoothing='stupid_backoff', unknown_handle=None, threshold=None, k=None, top_k_vocab=None),
+
+        "Bigram Unsmooth": NgramModel(ngram=2, smoothing=None, unknown_handle=None, threshold=None, k=None, top_k_vocab=None),
+        "Bigram Smooth with Laplace": NgramModel(ngram=2, smoothing='add-k', unknown_handle=None, threshold=None, k=1, top_k_vocab=None),
+        "Bigram Smooth with stupid_backoff": NgramModel(ngram=2, smoothing='stupid_backoff', unknown_handle=None, threshold=None, k=None, top_k_vocab=None),
     }
 
     for name, model in models.items():
@@ -89,7 +110,8 @@ def main():
             print("Unigram Probability of 'the':", model.unigram_probability("the"))
         else:
             print("Bigram Probability of ('the', 'students'):", model.bigram_probability("the", "students"))
-        print("Perplexity on validation set:", model.perplexity("val.txt"))
+        if model.smoothing is not None:
+            print("Perplexity on validation set:", model.perplexity("val.txt"))
         print()
 
 if __name__ == "__main__":
